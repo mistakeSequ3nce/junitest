4bc9094141b5725fa2cd219b9f9358d88b5a97c7
Alexandr <jozeyts@gmail.com>
2025-01-27 15:46:07 +0300

    v-0.3

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,10 @@
 <script setup>
-import {
-  HomeIcon,
-  IdentificationIcon,
-  ListBulletIcon,
-  ChatBubbleLeftIcon,
-} from '@heroicons/vue/24/outline'
-
-const mainPage = 'Главная страница'
-const postsListPage = 'Список постов'
-const usersListPage = 'Список пользователей'
-const commentsPage = 'Комментарии'
-const navItems = [mainPage, postsListPage, usersListPage, commentsPage]
+import TheHeader from './components/TheHeader.vue'
+import TheNav from './components/TheNav.vue'
 </script>
 
 <template>
-  <header class="TheHeader">
-    <a href="#">
-      <img
-        src="./assets/1724866703chatgpt-logo-with-no-background.png"
-        alt="logo"
-        class="TheLogo"
-      />
-    </a>
-  </header>
+  <TheHeader />
   <main class="PageMain"></main>
-  <nav class="TheNav">
-    <ul class="TheNavList">
-      <li v-for="page in navItems" :key="page">
-        <a :href="`#${page}`" class="TheNavListObject">
-          <HomeIcon v-if="page == mainPage" class="TheNavListIcons" />
-          <ListBulletIcon v-else-if="page == postsListPage" class="TheNavListIcons" />
-          <IdentificationIcon v-else-if="page == usersListPage" class="TheNavListIcons" />
-          <ChatBubbleLeftIcon v-else class="TheNavListIcons" /> {{ page }}
-        </a>
-      </li>
-    </ul>
-  </nav>
+  <TheNav />
 </template>
---
+++ src/components/TheNav.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {
+  HomeIcon,
+  IdentificationIcon,
+  ListBulletIcon,
+  ChatBubbleLeftIcon,
+} from '@heroicons/vue/24/outline'
+
+const mainPage = 'Главная страница'
+const postsListPage = 'Список постов'
+const usersListPage = 'Список пользователей'
+const commentsPage = 'Комментарии'
+const navItems = [mainPage, postsListPage, usersListPage, commentsPage]
+</script>
+
+<template>
+  <nav class="TheNav">
+    <ul class="TheNavList">
+      <li v-for="page in navItems" :key="page">
+        <a :href="`#${page}`" class="TheNavListObject">
+          <HomeIcon v-if="page == mainPage" class="TheNavListIcons" />
+          <ListBulletIcon v-else-if="page == postsListPage" class="TheNavListIcons" />
+          <IdentificationIcon v-else-if="page == usersListPage" class="TheNavListIcons" />
+          <ChatBubbleLeftIcon v-else class="TheNavListIcons" /> {{ page }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
